89bfda275eaa81b1146004c1e1f56c62fd19786e
shubham <shubhamagr281999@gmail.com>
2021-03-18 23:42:56 +0530

    developing the script file

--- drone_path_planner/script/movement.py
+++ drone_path_planner/script/movement.py
@@ -1,20 +1,51 @@
 #!/usr/bin/env python
-# license removed for brevity
+
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from time import sleep
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+v=2       #reference linear velocity
+w=1.25    #reference angular velocity
+x_pose=0.0f;
+y_pose=0.0f;
+z_pose=0.0f;
+roll=0.0f;
+pitch=0.0f
+yaw=0.0f;
+
+def move_forward(delta):
+    
+
+def rotate(w):
+    pub=rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size=10)
+    rospy.init_node('drone_move',anonymous=True)
+    vel=Twist()
+    vel.linear.x=vel.linear.y=vel.linear.z=0
+    vel.angular.x=vel.angular.y=0
+    vel.angular.z=w
+    
+    t0=rospy.Time.now().to_sec()
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+        pub.publish(vel)
+        if rospy.Time.now().to_sec()>1+t0:
+            break 
 
-if __name__ == '__main__':
-    try:
-        talker()
+    vel.angular.z=0
+    pub.publish(vel)
+
+def move():
+    pub_set_point_local=rospy.Publisher('/mavros/setpoint_raw/local',Twist,queue_size=10)
+    rospy.Subscriber("/mavros/global_position/local",nav_msgs/Odometry,gps_data_callback)
+    rospy.init_node('drone_move',anonymous=True)
+    vel=Twist()
+    vel.linear.x=vel.linear.y=vel.linear.z=0
+    vel.angular.x=vel.angular.y=vel.angular.z=0
+    pub.publish(vel)
+    sleep(1)
+    #movement algo comes here
+
+if __name__=="__main__":
+   `try:
+       move()
     except rospy.ROSInterruptException:
         pass
