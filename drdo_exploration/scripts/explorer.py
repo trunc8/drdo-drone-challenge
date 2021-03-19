9b78d6c03b609a284ea8a483d3d64ded3d4510b2
Siddharth Saha <sahasiddharth611@gmail.com>
2021-03-20 00:48:13 +0530

    Changed directory name from drdo-exploration to drdo_exploration

--- drdo_exploration/scripts/explorer.py
+++ drdo_exploration/scripts/explorer.py
@@ -11,6 +11,8 @@ from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 
+import scipy.ndimage
+
 class Exploration:
   def __init__(self):
 
@@ -25,6 +27,7 @@ class Exploration:
     rospy.Subscriber(pc2_topic, PointCloud2, self.pc2Callback)
     rospy.Subscriber(pose_topic, Odometry, self.positionCallback)
 
+
   def positionCallback(self, local_pose_msg):
     self.curr_position = [local_pose_msg.pose.pose.position.x,
                           local_pose_msg.pose.pose.position.y,
@@ -37,6 +40,7 @@ class Exploration:
     self.curr_orientation = tf.transformations.euler_from_quaternion(quaternion)
     # print(self.curr_position, self.curr_orientation)
 
+
   def pc2Callback(self, pc2_msg):
     xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pc2_msg)
     mask = (xyz_array[:,2] < 4.5) & (xyz_array[:,2] > 0.5)
@@ -45,6 +49,7 @@ class Exploration:
     # print(max(dist))
     # print("Received")
     # print(len(xyz_array))
+
 
   def pc2ImageCallback(self, pc2_img_msg):
     bridge = CvBridge()
@@ -56,24 +61,75 @@ class Exploration:
       return
     
     cv_image_array = np.array(cv_img, dtype = np.dtype('f8'))
-    i = 400
-    print(np.min(cv_image_array[i]))
-    print(np.max(cv_image_array[i]))
-    print(cv_image_array[i].shape)
-
     cv_image_norm = cv_image_array/10
-    cv2.imshow("Depth raw image", cv_image_norm)
-    cv2.waitKey(3)
-    # cv_image_norm = cv2.normalize(cv_image_array, cv_image_array, 0, 1, cv2.NORM_MINMAX)
-    # # Resize to the desired size
-    # cv_image_resized = cv2.resize(cv_image_norm, (pc2_img_msg.width, pc2_img_msg.height), interpolation = cv2.INTER_CUBIC)
-    # depthimg = np.array(cv_image_resized)
 
+    ## TODO: Filtering sky and ground ==> dont_see_mask
 
+    cleaned_cv_img = cv_image_norm.copy()
+    cleaned_cv_img[np.isnan(cleaned_cv_img)] = 1.0
+    bloated_cv_img = self.bloatImage(cleaned_cv_img)
     
-    # print(cv_image_norm)
+    cv2.imshow("Bloated img", bloated_cv_img)
+    cv2.waitKey(3)
+
+    max_intensity = bloated_cv_img.max()
+    print(max_intensity)
 
 
+  def bloatImage(self, cleaned_cv_img):
+    bloated_cv_img = cleaned_cv_img.copy()
+    safety_threshold = 15 # in pixels (needs tuning)
+    
+    '''
+    Calculate horizontal differences only finding decreasing brightnesses
+    ----------
+    Decreasing brightness => Brighter(farther) to darker(closer)
+    So danger obstacle is on the right of the edge line
+    '''
+    left_vertical_edge = cleaned_cv_img[:,0:-1] - cleaned_cv_img[:,1:]
+    left_vertical_edge = left_vertical_edge.clip(min=0)
+    left_vertical_mask = left_vertical_edge > 0.1
+    kernel = np.concatenate((np.ones(safety_threshold//2),
+                            np.zeros(safety_threshold//2)))
+    left_vertical_mask = scipy.ndimage.convolve1d(left_vertical_mask, weights=kernel, axis=1)
+    left_vertical_mask = left_vertical_mask > 0.1
+
+    bloated_cv_img[:,1:][left_vertical_mask] = 0
+
+    # print(np.min(left_vertical_edge[i]))
+    # print(np.max(left_vertical_edge[i]))
+    # print(left_vertical_edge[i].shape)
+    # print(left_vertical_mask.shape)
+
+    # cv2.imshow("Left Vertical Edge", left_vertical_edge)
+    # cv2.waitKey(3)
+
+
+    '''
+    Calculate horizontal differences only finding increasing brightnesses
+    ----------
+    Increasing brightness => Darker(closer) to brighter(farther)
+    So danger obstacle is on the left of the edge line
+    '''
+    right_vertical_edge = cleaned_cv_img[:,1:] - cleaned_cv_img[:,0:-1]
+    right_vertical_edge = right_vertical_edge.clip(min=0)
+    right_vertical_mask = right_vertical_edge > 0.1
+    kernel = np.concatenate((np.zeros(safety_threshold//2),
+                            np.ones(safety_threshold//2)))
+    right_vertical_mask = scipy.ndimage.convolve1d(right_vertical_mask, weights=kernel, axis=1)
+    right_vertical_mask = right_vertical_mask > 0.1
+
+    bloated_cv_img[:,0:-1][right_vertical_mask] = 0
+
+    # print(np.min(right_vertical_edge[i]))
+    # print(np.max(right_vertical_edge[i]))
+    # print(right_vertical_edge[i].shape)
+    
+    # cv2.imshow("Right Vertical Edge", right_vertical_edge)
+    # cv2.waitKey(3)
+
+    return bloated_cv_img
+    
 
 if __name__ == '__main__':
   try:
