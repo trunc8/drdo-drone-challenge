e8971b9b1b3430756ba00bd118b46f7dbde8f282
shubham <shubhamagr281999@gmail.com>
2021-03-19 19:14:25 +0530

    made changes to make the drone arm and take off on it own once node is run

--- drone_path_planner/scripts/movement.py
+++ drone_path_planner/scripts/movement.py
@@ -2,14 +2,49 @@
 import rospy
 ##from hector_uav_msgs.msg import PoseActionGoal
 ##from geometry_msgs import PoseStamped
-
+from time import sleep
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point,Twist
 from geometry_msgs.msg import PoseStamped
 from math import atan2, cos, sin
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import *
 from drone_path_planner.msg import teleopData
+from mavros_msgs.srv import SetMode, CommandBool, CommandTOL
 #from geometry_msgs import PoseStamped
+def setStabilizeMode():
+    rospy.wait_for_service('/mavros/set_mode')    
+
+    try:
+        isModeChanged_guided= False 
+        rate2=rospy.Rate(1)
+        while not isModeChanged_guided :            
+            flightModeService = rospy.ServiceProxy('/mavros/set_mode',SetMode)
+            isModeChanged = flightModeService(custom_mode='STABILIZE') #return true or false
+            isModeChanged_guided = flightModeService(custom_mode='GUIDED') #return true or false
+            rate2.sleep()
+    except rospy.ServiceException:
+        print "service set_mode call failed. GUIDED Mode could not be set. Check that GPS is enabled"
+def setArm():
+    rospy.wait_for_service('/mavros/cmd/arming')    
+    try:
+        armService= False
+        rate3=rospy.Rate(1)
+        while not armService :     
+            arming = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+            armService = arming(1)
+            if armService=='True':
+                rospy.loginfo("set arm enabled")
+            rate3.sleep()        
+    except rospy.ServiceException:
+        print("Service arm call failed")
+def setTakeoffMode():
+    rospy.wait_for_service('/mavros/cmd/takeoff')
+    try:        
+        takeoffService = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
+        takeoffService(altitude = 3.0, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
+        rospy.loginfo("Service takeoff call successful")
+    except rospy.ServiceException:
+        print ("Service takeoff call failed")
 class navigation:
     def __init__(self):
         self.rate=rospy.Rate(1)
@@ -32,7 +67,6 @@ class navigation:
         self.x_pose = msg.pose.pose.position.x   
         self.y_pose = msg.pose.pose.position.y   
         self.z_pose = msg.pose.pose.position.z
-        print(self.z_pose)
         rot_q =msg.pose.pose.orientation
         self.msgp.pose.orientation=msg.pose.pose.orientation
         (self.roll ,self.pitch ,self.theta)=euler_from_quaternion([rot_q.x ,rot_q.y,rot_q.z ,rot_q.w])
@@ -64,15 +98,17 @@ class navigation:
                 self.pub_set_point_local.publish(self.msgp)
                 self.decision=0
             self.rate.sleep()
-
         
 
 if __name__ == '__main__':
   rospy.init_node('navigator_node')
   rospy.loginfo("navigator_node created")
   try:
-    navigation_obj = navigation()    
- 
+    navigation_obj = navigation()  
+    setStabilizeMode()
+    setArm()
+    setTakeoffMode()
+    rospy.loginfo("Flight take off done successful and is ready for accepting furthur commands")
     ## Wait until node has loaded completely
     while (rospy.get_time()==0):
       pass
