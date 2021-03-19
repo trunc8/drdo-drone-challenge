c42e70b4c01cd5adb9f6b24a71e091752967deb4
Siddharth Saha <sahasiddharth611@gmail.com>
2021-03-20 03:57:31 +0530

    Got best candidate pixel for same elevation assumption; added direction.msg for publishing vector direction

--- drdo_exploration/scripts/explorer.py
+++ drdo_exploration/scripts/explorer.py
@@ -11,6 +11,7 @@ from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 
+import random
 import scipy.ndimage
 
 class Exploration:
@@ -19,6 +20,7 @@ class Exploration:
     self.curr_position = None
     self.curr_orientation = None
     self.init_pose = None
+    self.pc2_arr = None
 
     pc2_topic = '/depth_camera/depth/points'
     pose_topic = '/mavros/global_position/local'
@@ -40,15 +42,18 @@ class Exploration:
     self.curr_orientation = tf.transformations.euler_from_quaternion(quaternion)
     # print(self.curr_position, self.curr_orientation)
 
-
   def pc2Callback(self, pc2_msg):
-    xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pc2_msg)
-    mask = (xyz_array[:,2] < 4.5) & (xyz_array[:,2] > 0.5)
-    xyz_array = xyz_array[mask]
-    dist = np.linalg.norm(xyz_array, axis=1)
-    # print(max(dist))
-    # print("Received")
-    # print(len(xyz_array))
+    self.pc2_arr = ros_numpy.numpify(pc2_msg)
+
+
+  # def pc2Callback(self, pc2_msg):
+  #   xyz_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pc2_msg)
+  #   mask = (xyz_array[:,2] < 4.5) & (xyz_array[:,2] > 0.5)
+  #   xyz_array = xyz_array[mask]
+  #   dist = np.linalg.norm(xyz_array, axis=1)
+  #   # print(max(dist))
+  #   # print("Received")
+  #   # print(len(xyz_array))
 
 
   def pc2ImageCallback(self, pc2_img_msg):
@@ -61,24 +66,57 @@ class Exploration:
       return
     
     cv_image_array = np.array(cv_img, dtype = np.dtype('f8'))
-    cv_image_norm = cv_image_array/10
+    POINTCLOUD_CUTOFF = 10
+    cv_image_norm = cv_image_array/POINTCLOUD_CUTOFF    
 
     ## TODO: Filtering sky and ground ==> dont_see_mask
 
     cleaned_cv_img = cv_image_norm.copy()
     cleaned_cv_img[np.isnan(cleaned_cv_img)] = 1.0
-    bloated_cv_img = self.bloatImage(cleaned_cv_img)
-    
-    cv2.imshow("Bloated img", bloated_cv_img)
-    cv2.waitKey(3)
 
-    max_intensity = bloated_cv_img.max()
-    print(max_intensity)
+    bloated_cv_img = self.bloatImage(cleaned_cv_img)
+    target = self.findTarget(bloated_cv_img)
+    
+
+
+  def findTarget(self, bloated_cv_img):
+    '''
+    Find (u,v) pixel coordinates that's the
+    best candidate for target
+    '''
+    height, width = bloated_cv_img.shape
+    max_intensity = np.max(bloated_cv_img)
+    candidates = bloated_cv_img == max_intensity
+    candidates = candidates.astype(float)
+
+    BAND_WIDTH = 5
+    band_mask = np.zeros(bloated_cv_img.shape, dtype=bool)    
+    band_mask[height//2-BAND_WIDTH:height//2+BAND_WIDTH,:] = 1
+
+    candidates_in_band = np.zeros(bloated_cv_img.shape)
+    candidates_in_band = np.multiply(band_mask, candidates)
+
+    
+    if not np.any(candidates_in_band):
+      '''
+      TODO
+      All zeros, can't maintain same elevation for drone
+      '''
+      target = np.zeros(2)
+    else:
+      nonzero_candidates = candidates_in_band.nonzero()
+      idx = random.randint(0, len(nonzero_candidates[0])-1)
+      target = np.array([nonzero_candidates[0][idx],
+                         nonzero_candidates[1][idx]])
+
+    cv2.imshow("Bloated img", candidates_in_band)
+    cv2.waitKey(3)
+    return target
 
 
   def bloatImage(self, cleaned_cv_img):
     bloated_cv_img = cleaned_cv_img.copy()
-    safety_threshold = 15 # in pixels (needs tuning)
+    SAFETY_THRESHOLD = 15 # in pixels (needs tuning)
     
     '''
     Calculate horizontal differences only finding decreasing brightnesses
@@ -89,8 +127,8 @@ class Exploration:
     left_vertical_edge = cleaned_cv_img[:,0:-1] - cleaned_cv_img[:,1:]
     left_vertical_edge = left_vertical_edge.clip(min=0)
     left_vertical_mask = left_vertical_edge > 0.1
-    kernel = np.concatenate((np.ones(safety_threshold//2),
-                            np.zeros(safety_threshold//2)))
+    kernel = np.concatenate((np.ones(SAFETY_THRESHOLD//2),
+                            np.zeros(SAFETY_THRESHOLD//2)))
     left_vertical_mask = scipy.ndimage.convolve1d(left_vertical_mask, weights=kernel, axis=1)
     left_vertical_mask = left_vertical_mask > 0.1
 
@@ -114,8 +152,8 @@ class Exploration:
     right_vertical_edge = cleaned_cv_img[:,1:] - cleaned_cv_img[:,0:-1]
     right_vertical_edge = right_vertical_edge.clip(min=0)
     right_vertical_mask = right_vertical_edge > 0.1
-    kernel = np.concatenate((np.zeros(safety_threshold//2),
-                            np.ones(safety_threshold//2)))
+    kernel = np.concatenate((np.zeros(SAFETY_THRESHOLD//2),
+                            np.ones(SAFETY_THRESHOLD//2)))
     right_vertical_mask = scipy.ndimage.convolve1d(right_vertical_mask, weights=kernel, axis=1)
     right_vertical_mask = right_vertical_mask > 0.1
 
