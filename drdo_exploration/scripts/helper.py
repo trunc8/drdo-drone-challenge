77d01dd6ea5338c9d6da4642f74f32b14c41fcf3
Siddharth Saha <sahasiddharth611@gmail.com>
2021-03-22 22:59:20 +0530

    Cleaned up helper.py a little

--- drdo_exploration/scripts/helper.py
+++ drdo_exploration/scripts/helper.py
@@ -109,6 +109,7 @@ class Helper:
 
     return cleaned_cv_img
 
+
   def pixel_to_dirn(self, h, w):
     height, width = [480, 640]
     target_px = np.array([h-height//2, w-width//2])
@@ -131,22 +132,6 @@ class Helper:
     # return mat
     return ps
 
-  def pixel_to_depth(self, h, w):     #h,w are image coordinates
-    # print(h,w)
-    
-    xp = self.pc2_arr['x'][h][w]
-    yp = self.pc2_arr['y'][h][w]
-    zp = self.pc2_arr['z'][h][w]  
-
-
-    ps = PointStamped()
-    ps.header.frame_id = "depth_cam_link"
-    ps.header.stamp = rospy.Time(0)
-    ps.point.x = zp
-    ps.point.y = -xp
-    ps.point.z = -yp
-    mat = self.listener.transformPoint("/map", ps)
-    return mat
 
   def findTarget(self, penalized_cv_img):
     '''
@@ -163,11 +148,8 @@ class Helper:
     x_values = nonzero_candidates[1]
     length = len(x_values)
 
+    # Finding median by x pixel position
     idx = np.argpartition(x_values, len(x_values) // 2)[len(x_values) // 2]
-    # sorted_x_values = np.sort(x_values)
-    # coupled_y_values = y_values(x_values.tolist().argsort())
-    # target = np.array(
-
 
     # idx = random.randint(0, len(nonzero_candidates[0])-1)
     target = np.array([nonzero_candidates[0][idx],
@@ -178,7 +160,7 @@ class Helper:
   def penalizeObstacleProximity(self, cleaned_cv_img):
     penalized_cv_img = cleaned_cv_img.copy()
     
-    
+    #---------------------------------------------------------#
     '''
     Calculate horizontal differences only finding increasing brightnesses
     ----------
@@ -206,18 +188,20 @@ class Helper:
     # This matrix is basically blips at the pixels of left_vertical_edge
 
     left_vertical_penalty = self.K_vertical*scipy.ndimage.convolve1d(left_vertical_mask,
-          weights= self.kernel_left, axis=1)
+          weights= self.kernel_left, mode='constant', cval=0, axis=1)
     
-    penalized_cv_img[:,0:-1] = penalized_cv_img[:,0:-1] - right_vertical_penalty
-    penalized_cv_img[:,1:] = penalized_cv_img[:,1:] - left_vertical_penalty
+    # penalized_cv_img[:,0:-1] = penalized_cv_img[:,0:-1] - right_vertical_penalty
+    # penalized_cv_img[:,1:] = penalized_cv_img[:,1:] - left_vertical_penalty
     # penalized_cv_img = penalized_cv_img.clip(min=0)
 
-    penalized_cv_img[:,0] = np.zeros(480)
-    penalized_cv_img[:,-1] = np.zeros(480)
+    # penalized_cv_img[:,0] = np.zeros(480)
+    # penalized_cv_img[:,-1] = np.zeros(480)
     # print(penalized_cv_img[0,400:])
     # cv2.imshow("Penalized image", penalized_cv_img)
     # cv2.waitKey(3)
 
+
+    #---------------------------------------------------------#
     ## Penalize distance from horizontal centerline
 
     y_dist_penalty = np.arange(480) - 479/2.
@@ -227,13 +211,11 @@ class Helper:
     
     
     y_dist_penalty = self.K_cam*y_dist_penalty/(np.max(y_dist_penalty))
-    penalized_cv_img = penalized_cv_img - y_dist_penalty
-    penalized_cv_img.clip(min=0)
+    # penalized_cv_img = penalized_cv_img - y_dist_penalty
+    # penalized_cv_img.clip(min=0)
 
-
-    ## Penalize deviation of z-coordinate from self.Z_REF
-
-    
+    #---------------------------------------------------------#
+    ## Penalize deviation of z-coordinate from self.Z_REF    
 
     err = (self.curr_position[2]-self.Z_REF)/self.Z_REF
     z_penalty = self.K_altitude*np.arange(480)*np.abs(err)/480
@@ -241,148 +223,27 @@ class Helper:
       z_penalty = z_penalty[::-1]
 
     z_penalty = np.matlib.repmat(z_penalty,640,1).T
-    penalized_cv_img = penalized_cv_img - z_penalty
-    penalized_cv_img.clip(min=0)
+    # penalized_cv_img = penalized_cv_img - z_penalty
+    # penalized_cv_img.clip(min=0)
 
 
     # cv2.imshow("Y dist penalty", penalized_cv_img)
     # cv2.waitKey(3)
 
+    penalized_cv_img[:,0:-1] = penalized_cv_img[:,0:-1] - right_vertical_penalty
+    penalized_cv_img[:,1:] = penalized_cv_img[:,1:] - left_vertical_penalty
+    penalized_cv_img[:,0] = np.zeros(480)
+    penalized_cv_img[:,-1] = np.zeros(480)
+    
+    penalized_cv_img = penalized_cv_img - y_dist_penalty
+
+    penalized_cv_img = penalized_cv_img - z_penalty
+
+    penalized_cv_img.clip(min=0)
+
+
+
     return penalized_cv_img
 
 
   
-  def collision_avoidance(self, cleaned_cv_img ):
-
-    collision_cv_img = cleaned_cv_img.copy()
-    
-    right_vertical_edge = cleaned_cv_img[:,1:] - cleaned_cv_img[:,0:-1]
-    right_vertical_edge = right_vertical_edge.clip(min=0)
-    right_vertical_mask = (right_vertical_edge > 0.1).astype(float)
-
-
-  
-    left_vertical_edge = cleaned_cv_img[:,0:-1] - cleaned_cv_img[:,1:]
-    left_vertical_edge = left_vertical_edge.clip(min=0)
-    left_vertical_mask = (left_vertical_edge > 0.1).astype(float)
-   
-    edges_img_right = np.zeros(collision_cv_img.shape)
-    edges_img = np.zeros(collision_cv_img.shape)
-    edges_img_left = np.zeros(collision_cv_img.shape)
-
-    edges_img_right[:,0:-1] = right_vertical_mask
-    edges_img_left[:,1:] = left_vertical_mask
-    edges_img = np.logical_or(edges_img_left,edges_img_right)
-    
-    #depth = np.logical_and(collision_cv_img,yo)
-    
-    projected_1_d_array = np.amax(edges_img , axis=0)
-    #print("projected_1d_array shape",projected_1_d_array.shape)
-    #print(projected_1_d_array)
-    col_edges = np.where(projected_1_d_array==1) #return (arr[196,509],);flatten array #np.ndarray.flatten
-    #print("col_edges[0]",col_edges[0])
-    FOCAL_LENGTH = 554.25
-    DRONE_SIZE = 0.5  #exact value 0.47
-
-    for i in col_edges[0]:  
-      print("i",i)                                    
-      row_edge = np.where(edges_img[:,i] == 1) 
-      #print("row_edge",row_edge)
-      #print("row_edge",row_edge[0])
-      print("row_edge_first",row_edge[0][0])
-      print("row_edge_last" , row_edge[0][-1])
-      #cv2.imshow("collision_cv_img",collision_cv_img)
-
-      image_plane_distance = (collision_cv_img[row_edge[0][0],i]+ 1e-1) * self.POINTCLOUD_CUTOFF
-      #image_plane_distance = depth_value
-      drone_size_projected = (DRONE_SIZE/2)*FOCAL_LENGTH/image_plane_distance
-      
-      drone_proj_left = i - drone_size_projected
-      drone_proj_right = i + drone_size_projected
-
-      print("drone_size_projected",drone_size_projected)
-
-      edges_img[ row_edge[0][0]:row_edge[0][-1] , int(drone_proj_left):int(drone_proj_right) ] = 1
-
-      collision_cv_img = np.multiply(collision_cv_img , (1-edges_img))
-      
-
-  
-      
-    
-
-    #print(b)
-
-    #print(depth[:,b])
-    # dest-(h,w)
-
-    #cleaned_cv_img*10 along the edges detected = depth value>
-    #image_plane distance = depth value>
-    #FOCAL_LENGTH = 554.25
-    #DRONE_SIZE = 47cm
-    # drone_proj_left = w - (DRONE_SIZE//2)*fl/image_plane_dis
-    # drone_proj_right = w + (DRONE_SIZE//2)*fl/image_plane_dis
-    # spanned over the whole column
-
-
-  
-    
-
-
-
-
-    # collision_cv_img[:,0:-1] = np.multiply(collision_cv_img[:,0:-1], (1-right_vertical_mask))
-    
-    # #collision_cv_img[:,1:][left_vertical_mask] = 0
-    # collision_cv_img[:,0:-1] = np.multiply(collision_cv_img[:,0:-1], (1-left_vertical_mask))
-    #cv2.imshow("cleaned_cv_img",collision_cv_img)
-    # cv2.imshow("right_vertical_mask",right_vertical_mask)
-    # cv2.imshow("left_vertical_mask",left_vertical_mask)
-    # cv2.imshow("vertical edges", vertical_edges)
-    #cv2.imshow("vertical_edges",yo.astype(float))
-    # cv2.imshow("collision_cv_img" , collision_cv_img)
-
-    # cv2.waitKey(3)
-    return collision_cv_img
-
-
-
-
-
-  # def bloatImage(self, cleaned_cv_img):
-  #   bloated_cv_img = cleaned_cv_img.copy()
-  #   SAFETY_THRESHOLD = 200 # in pixels (needs tuning)
-    
-  #   '''
-  #   Calculate horizontal differences only finding decreasing brightnesses
-  #   ----------
-  #   Decreasing brightness => Brighter(farther) to darker(closer)
-  #   So danger obstacle is on the right of the edge line
-  #   '''
-  #   left_vertical_edge = cleaned_cv_img[:,0:-1] - cleaned_cv_img[:,1:]
-  #   left_vertical_edge = left_vertical_edge.clip(min=0)
-  #   left_vertical_mask = left_vertical_edge > 0.1
-  #   kernel = np.concatenate((np.ones(SAFETY_THRESHOLD//2),
-  #                           np.zeros(SAFETY_THRESHOLD//2)))
-  #   left_vertical_mask = scipy.ndimage.convolve1d(left_vertical_mask, weights=kernel, axis=1)
-  #   left_vertical_mask = left_vertical_mask > 0.1
-
-  #   bloated_cv_img[:,1:][left_vertical_mask] = 0
-
-  #   '''
-  #   Calculate horizontal differences only finding increasing brightnesses
-  #   ----------
-  #   Increasing brightness => Darker(closer) to brighter(farther)
-  #   So danger obstacle is on the left of the edge line
-  #   '''
-  #   right_vertical_edge = cleaned_cv_img[:,1:] - cleaned_cv_img[:,0:-1]
-  #   right_vertical_edge = right_vertical_edge.clip(min=0)
-  #   right_vertical_mask = right_vertical_edge > 0.1
-  #   kernel = np.concatenate((np.zeros(SAFETY_THRESHOLD//2),
-  #                           np.ones(SAFETY_THRESHOLD//2)))
-  #   right_vertical_mask = scipy.ndimage.convolve1d(right_vertical_mask, weights=kernel, axis=1)
-  #   right_vertical_mask = right_vertical_mask > 0.1
-
-  #   bloated_cv_img[:,0:-1][right_vertical_mask] = 0
-
-  #   return bloated_cv_img
