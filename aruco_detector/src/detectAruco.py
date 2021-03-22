0d04dd9fafc5f85e58ec4904f81c4d8e969b34bb
Shubham <shubhi.g2001@gmail.com>
2021-03-23 01:03:14 +0530

    changes made in detectAruco

--- aruco_detector/src/detectAruco.py
+++ aruco_detector/src/detectAruco.py
@@ -23,7 +23,7 @@ from std_msgs.msg import Int16
 first1 = True
 first2 = True
 counter1 = 1
-flag_zero_detected = 0
+flag_zero_detected = False
 
 def loadImages(data):
 
@@ -35,8 +35,10 @@ def loadImages(data):
      global first1
      global counter1
      global flag_zero_detected
+
      counter1 = counter1%300
      counter1 += 1 #To count how many number of times this function is called
+
 
      ## Converting uchar image matrix to int 
      for i in data.data:
@@ -50,7 +52,8 @@ def loadImages(data):
      arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_1000)
      arucoParams = cv2.aruco.DetectorParameters_create()
      imgMat = imgMat.astype(np.uint8)
-     imgMat = cv2.medianBlur(imgMat,3)
+     # imgMat = cv2.GaussianBlur(imgMat,(5,5),0)
+     # imgMat = cv2.medianBlur(imgMat,3)
      (corners, ids, rejected) = cv2.aruco.detectMarkers(imgMat, arucoDict,parameters=arucoParams)
 
 
@@ -58,8 +61,6 @@ def loadImages(data):
 
      if ids is None: ##If no marker is detected
           print("FINDING MARKERS!!!")
-
-
 
      else:
           flag_stop = Int16()
@@ -81,30 +82,32 @@ def loadImages(data):
           for i in range(len(ids)):
                if ids[i][0] == 0:
                     markerIndex = i
-               
+
+          
+           
           if markerIndex is not None: #Only if id 0 is found
 
-               flag_zero_detected = 1
+               flag_zero_detected = True
 
                #Calculate the center of the aruco marker
                center = (corners[markerIndex][0][0] + corners[markerIndex][0][1] + corners[markerIndex][0][2] + corners[markerIndex][0][3])/4.0
           
                #Get the scaled distance to the center of the aruco marker
                lengthOfEdgeInPixel = math.sqrt((corners[markerIndex][0][0][0] - corners[markerIndex][0][1][0])**2 + (corners[markerIndex][0][0][1] - corners[markerIndex][0][1][1])**2)
-               scaleFactor = 0.13/lengthOfEdgeInPixel
+               scaleFactor = 0.32/lengthOfEdgeInPixel
                thresholdFactor = 0.1778/lengthOfEdgeInPixel
                translationVectorInPixels = [(center[0] - 320),(center[1] - 240)] #Translation in terms of pixels
                translationVectorDrone = [-scaleFactor*translationVectorInPixels[1],scaleFactor*translationVectorInPixels[0]] # Actual translation distance
 
                ##Optimize distance in X
-               if((abs(translationVectorInPixels[0]) > 0.1/thresholdFactor) and counter1%3 == 0):
+               if((abs(translationVectorInPixels[0]) > 0.1/thresholdFactor) and counter1%8 == 0 and abs(translationVectorInPixels[0]) > abs(translationVectorInPixels[1])):
                     msg.decision = 2
                     msg.delta = translationVectorDrone[1]
                     print(translationVectorDrone[1])
                     pub.publish(msg)
                
                ##Optimize distance in y
-               elif((abs(translationVectorInPixels[1]) > 0.1/thresholdFactor) and counter1%3 == 0):
+               elif((abs(translationVectorInPixels[1]) > 0.1/thresholdFactor) and counter1%8 == 0):
                     msg.decision = 1
                     msg.delta = translationVectorDrone[0]
                     print(translationVectorDrone[0])
@@ -112,26 +115,26 @@ def loadImages(data):
                
                ##Once both x and y get optimized, decrease the altitude
                elif((abs(translationVectorInPixels[0]) < 0.1/thresholdFactor) and (abs(translationVectorInPixels[1]) < 0.1/thresholdFactor) and first1):
-                    # msg.decision = 4
-                    # msg.delta = 1.5
-                    # pub.publish(msg)
+                    msg.decision = 4
+                    msg.delta = 1.5
+                    pub.publish(msg)
                     first1 = False
                     setStabilizeMode()
-
+     
 
           else:
                if not flag_zero_detected:
-                    center_grp  =0
+                    center_grp = 0
                     for i in range(len(ids)):          
                          center_grp =+ (corners[i][0][0] + corners[i][0][1] + corners[i][0][2] + corners[i][0][3])
      
-                    centre_grp /= 4*len(ids)
+                    center_grp /= 4*len(ids)
                     lengthOfEdgeInPixel_grp = math.sqrt((corners[0][0][0][0] - corners[0][0][1][0])**2 + (corners[0][0][0][1] - corners[0][0][1][1])**2)
-                    scaleFactor_grp = 0.13/lengthOfEdgeInPixel
-                    translationVectorInPixels_grp = [(center[0] - 320),(center[1] - 240)]
-                    translationVectorDrone_grp = [-scaleFactor*translationVectorInPixels[1],scaleFactor*translationVectorInPixels[0]]
+                    scaleFactor_grp = 0.2/lengthOfEdgeInPixel_grp
+                    translationVectorInPixels_grp = [(center_grp[0] - 320),(center_grp[1] - 240)]
+                    translationVectorDrone_grp = [-scaleFactor_grp*translationVectorInPixels_grp[1],scaleFactor_grp*translationVectorInPixels_grp[0]]
      
-                    if translationVectorInPixels_grp[1] > translationVectorInPixels_grp[0]:
+                    if abs(translationVectorInPixels_grp[1]) > abs(translationVectorInPixels_grp[0]):
                          msg.decision = 1
                          msg.delta = translationVectorDrone_grp[0]
                          pub.publish(msg)
@@ -173,7 +176,7 @@ def setStabilizeMode():
 if __name__ == '__main__':
 
      rospy.init_node('listener', anonymous=True)
-     pub = rospy.Publisher("/drone/teleop",teleopData)
-     pub_stop_explore = rospy.Publisher("/stop_exploring_flag", Int16)
+     pub = rospy.Publisher("/drone/teleop",teleopData, queue_size = 10)
+     pub_stop_explore = rospy.Publisher("/stop_exploring_flag", Int16, queue_size = 10)
      rospy.Subscriber("/camera/color/image_raw/", Image, loadImages)
      rospy.spin()
