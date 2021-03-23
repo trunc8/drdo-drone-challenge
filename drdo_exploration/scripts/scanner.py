561fe8c491835269b88518836bf2262e99f2932a
Pranav K Das <pranavkdas12@gmail.com>
2021-03-24 05:28:21 +0530

    fixed bad merge of scanner.py

--- drdo_exploration/scripts/scanner.py
+++ drdo_exploration/scripts/scanner.py
@@ -30,22 +30,24 @@ def callback_opencv(data):
 	cv2.circle(img,(img.shape[1]//2,img.shape[0]//2),4,(255,0,0),-1)
 
 	aruco = aruco_detect()
+	print("aruco",aruco)
 	aruco.cX = 0.0
 	aruco.cY = 0.0
 	aruco.distance = 0.0
 	aruco.edge_distance=0.0
 
 
+
 	if ids is None: ##If no marker is detected
 		print("FINDING MARKERS!!!")
-		# cv2.imshow("frame",img)
-		# cv2.waitKey(3)
+		cv2.imshow("frame",img)
+		cv2.waitKey(3)
 		aruco.flag = 0
 	else:
 		a = np.where(ids==0)  #tuple containing index of the aruco with id zero.
 		if a[0].size==0:
-			# cv2.imshow("frame",img)
-			# cv2.waitKey(3)
+			cv2.imshow("frame",img)
+			cv2.waitKey(3)
 			aruco.flag = 0
 		else:
 			print("Found markers. Moving towards it")
@@ -68,20 +70,12 @@ def callback_opencv(data):
 			cv2.circle(img, (cX, cY), 4, (0,255,0), -1)
 			# cv2.circle(img,(img.shape[1]//2,img.shape[0]//2),4,(255,0,0),-1)
 
-<<<<<<< HEAD
+
 			cv2.imshow("frame",img)
 			cv2.waitKey(3)
-			print ("Cx is ", cX)
-			print ("CY is ", cY)
-			print ("Image_shape_0",img.shape[0])
-			print ("Image_shape_0",img.shape[1])
-			edge_distance = sqrt( (topLeft[0]-topRight[0])**2 + (topLeft[0]-topRight[0])**2 )//2
-=======
-			# cv2.imshow("frame",img)
-			# cv2.waitKey(3)
 
-			#edge_distance = sqrt( (topLeft[0]-topRight[0])**2 + (topLeft[0]-topRight[0])**2 )//2
->>>>>>> aff46c525661423cd9965fa7492a567a80bc5ca0
+			edge_distance = sqrt( (topLeft[0]-topRight[0])**2 + (topLeft[0]-topRight[0])**2 )//2
+
 			distance = 	sqrt( (cX-img.shape[1]//2)**2 + (cY-img.shape[0]//2)**2 )
 			print(distance)
 			aruco.flag = 1
