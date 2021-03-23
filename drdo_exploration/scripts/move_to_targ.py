2ea4df21c1153fd587d0248030ffdb3c7863947d
pavanIITB <kalepavan1234@gmail.com>
2021-03-23 23:10:21 +0530

    aruco_detect node created

--- drdo_exploration/scripts/move_to_targ.py
+++ drdo_exploration/scripts/move_to_targ.py
@@ -14,6 +14,7 @@ from nav_msgs.msg import *
 from drdo_exploration.msg import direction #Here direction is the message containing target co-ordinates.
 from mavros_msgs.srv import SetMode, CommandBool, CommandTOL
 from std_msgs.msg import Int16
+from drdo_exploration.msg import aruco_detect
 #from geometry_msgs import PoseStamped
 
 import numpy as np
@@ -21,132 +22,166 @@ import numpy as np
 
 
 class moveCopter:
-    def __init__(self):
-        '''
-        All values are initialized to zero.
-        pub_set_point_local publishes the goal_point co-ordinates.
-        sub_gps and sub_targ_vector subscribes to the global co-ordinates of the drone and the local co-ordinates of the goal_point.
-        '''
-        self.x_pose=0.0
-        self.y_pose=0.0
-        self.z_pose=0.0
-        self.roll=0.0
-        self.pitch=0.0
-        self.yaw=0.0
-        self.targ_x=0.0
-        self.targ_y=0.0
-        self.targ_z=0.0
-        self.rel_yaw = 0.0
+		def __init__(self):
+				'''
+				All values are initialized to zero.
+				pub_set_point_local publishes the goal_point co-ordinates.
+				sub_gps and sub_targ_vector subscribes to the global co-ordinates of the drone and the local co-ordinates of the goal_point.
+				'''
+				self.x_pose=0.0
+				self.y_pose=0.0
+				self.z_pose=0.0
+				self.roll=0.0
+				self.pitch=0.0
+				self.yaw=0.0
+				self.targ_x=0.0
+				self.targ_y=0.0
+				self.targ_z=0.0
+				self.rel_yaw = 0.0
 
-        rospy.init_node('navigator_node')
-        self.pub_set_point_local=rospy.Publisher('/mavros/setpoint_position/local', PoseStamped,queue_size=10)
-        self.sub_gps=rospy.Subscriber("/mavros/global_position/local",Odometry, self.gps_data_callback)
-        self.flag_to_stop = rospy.Subscriber("/stop_exploring_flag", Int16, self.stop_moving_callback)
-        self.sub_targ_vector=rospy.Subscriber("/target_vector",direction, self.targ_vector_callback)
-        self.msgp=PoseStamped()
-        self.rate=rospy.Rate(1)
+				rospy.init_node('navigator_node')
+				self.pub_set_point_local=rospy.Publisher('/mavros/setpoint_position/local', PoseStamped,queue_size=10)
+				self.sub_gps=rospy.Subscriber("/mavros/global_position/local",Odometry, self.gps_data_callback)
+				self.sub_aruco_detect = rospy.Subscriber("/aruco_detect", aruco_detect, self.aruco_detect_callback)
+				self.sub_targ_vector=rospy.Subscriber("/target_vector",direction, self.targ_vector_callback)
+				self.msgp=PoseStamped()
+				self.rate=rospy.Rate(1)
 
-        self.e_prev = 0.0
-        self.t_prev = 0.0
-        self.I = 0.0
+				self.e_prev = 0.0
+				self.t_prev = 0.0
+				self.I = 0.0
 
-        
-      
-
-
-    def gps_data_callback(self,msg):
-        ''' 
-        msgp is of PoseStamped msg type that we need to publish for going to the goal_point.
-        the use of roll, pitch, and yaw needs to be looked into.
-        ''' 
-        self.x_pose = msg.pose.pose.position.x   
-        self.y_pose = msg.pose.pose.position.y   
-        self.z_pose = msg.pose.pose.position.z
-        rot_q =msg.pose.pose.orientation
-        self.msgp.pose.orientation=msg.pose.pose.orientation
-        (self.roll ,self.pitch ,self.yaw)=euler_from_quaternion([rot_q.x ,rot_q.y,rot_q.z ,rot_q.w])
+				
+			
 
 
-
-        #print("gps_data_callback: %.2fm %.2fm %.2f deg"%(self.x_pose, self.y_pose, self.yaw*180/3.14))
-        
-
-    def targ_vector_callback(self,msg):
-        '''
-        The local co-ordinates of the targets are read into the variables.
-        As soon as we get that, we call the move_to_target function also which commands drone to go to that point
-        '''
-        self.targ_x=msg.vec_x
-        self.targ_y=msg.vec_y
-        self.targ_z=msg.vec_z
-        self.rel_yaw = math.atan2(self.targ_y,self.targ_x)
-        # self.move_to_target()
-        # self.rate.sleep()
+		def gps_data_callback(self,msg):
+				''' 
+				msgp is of PoseStamped msg type that we need to publish for going to the goal_point.
+				the use of roll, pitch, and yaw needs to be looked into.
+				''' 
+				self.x_pose = msg.pose.pose.position.x   
+				self.y_pose = msg.pose.pose.position.y   
+				self.z_pose = msg.pose.pose.position.z
+				rot_q =msg.pose.pose.orientation
+				self.msgp.pose.orientation=msg.pose.pose.orientation
+				(self.roll ,self.pitch ,self.yaw)=euler_from_quaternion([rot_q.x ,rot_q.y,rot_q.z ,rot_q.w])
 
 
-    def move_to_target(self): 
-        '''
-        Here we find the final global co-ordinates by adding gps pose and message we figured out. 
-        '''
-        # print(self.msgp)
-        q = quaternion_from_euler(0, 0, self.yawPID())
-        # print(q)
-        delta = 1
-        delta_x = self.targ_x*np.cos(self.yaw)-self.targ_y*np.sin(self.yaw)
-        delta_y = self.targ_x*np.sin(self.yaw)+self.targ_y*np.cos(self.yaw)
-        delta_x = delta_x*delta
-        delta_y = delta_y*delta
-        self.msgp.pose.position.z=self.z_pose + self.targ_z*delta
-        self.msgp.pose.position.x=self.x_pose + delta_x
-        self.msgp.pose.position.y=self.y_pose + delta_y
-        
-        self.msgp.pose.orientation.x = q[0]
-        self.msgp.pose.orientation.y = q[1]
-        self.msgp.pose.orientation.z = q[2]
-        self.msgp.pose.orientation.w = q[3]
-        self.pub_set_point_local.publish(self.msgp)
-        # print("Target pose")
-        # print(self.msgp.pose.position.x, self.msgp.pose.position.y, self.msgp.pose.position.z)
 
-    def stop_moving_callback(self,msg):
-        flag_stop = msg.data
-        if not flag_stop:
-            self.move_to_target()
-        else:
-            rospy.signal_shutdown("Open Movement.py")
+				#print("gps_data_callback: %.2fm %.2fm %.2f deg"%(self.x_pose, self.y_pose, self.yaw*180/3.14))
+				
 
+		def targ_vector_callback(self,msg):
+				'''
+				The local co-ordinates of the targets are read into the variables.
+				As soon as we get that, we call the move_to_target function also which commands drone to go to that point
+				'''
+				self.targ_x=msg.vec_x
+				self.targ_y=msg.vec_y
+				self.targ_z=msg.vec_z
+				self.rel_yaw = math.atan2(self.targ_y,self.targ_x)
+				# self.move_to_target()
+				# self.rate.sleep()
 
-    def yawPID(self):
-    
-      Kp = 0.6
-      Kd = 0
-      Ki = 0
-      ERROR_THRESHOLD_FOR_INTEGRATOR = 0.2
-      WINDUP_THRESHOLD = 0.2
+		def aruco_detect_callback(self,msg):
+				'''
+				The local co-ordinates of the targets are read into the variables.
+				As soon as we get that, we call the move_to_target function also which commands drone to go to that point
+				'''
+				self.flag=msg.flag
+				self.cX=msg.cX
+				self.cY=msg.cY
+				self.distance=msg.distance
+				#print(msg)
 
-      e = self.rel_yaw
-      #print(e*180/3.14)
-      dt = 1
+				if (self.flag):				
+					print("Aruco Marker detected!")
+					print("Aligning with Aruco Marker")
+					Delta = self.distance/2000
 
-      P = Kp*e
-      if abs(e) < ERROR_THRESHOLD_FOR_INTEGRATOR and abs(self.I) < WINDUP_THRESHOLD:
-        self.I = self.I + Ki*e*(dt)
-      D = Kd*(e - self.e_prev)/(dt)
+					pose_msg = PoseStamped()
+					pose_msg.pose.position.x = self.x_pose + (self.cX-320.0)*Delta   
+					pose_msg.pose.position.y = self.y_pose - (self.cY-240.0)*Delta  
+					pose_msg.pose.position.z = self.z_pose
 
-      Yaw = self.yaw + P + self.I + D
+					self.pub_set_point_local.publish(pose_msg)
+					if (self.distance<50):
+						print("Landing")
+						self.setLandMode()
+				else:
+					self.move_to_target()
 
-      self.e_prev = e
-      return Yaw
-        
+		def move_to_target(self): 
+				'''
+				Here we find the final global co-ordinates by adding gps pose and message we figured out. 
+				'''
+				# print(self.msgp)
+				q = quaternion_from_euler(0, 0, self.yawPID())
+				# print(q)
+				delta = 1
+				delta_x = self.targ_x*np.cos(self.yaw)-self.targ_y*np.sin(self.yaw)
+				delta_y = self.targ_x*np.sin(self.yaw)+self.targ_y*np.cos(self.yaw)
+				delta_x = delta_x*delta
+				delta_y = delta_y*delta
+				self.msgp.pose.position.z=self.z_pose + self.targ_z*delta
+				self.msgp.pose.position.x=self.x_pose + delta_x
+				self.msgp.pose.position.y=self.y_pose + delta_y
+				
+				self.msgp.pose.orientation.x = q[0]
+				self.msgp.pose.orientation.y = q[1]
+				self.msgp.pose.orientation.z = q[2]
+				self.msgp.pose.orientation.w = q[3]
+				self.pub_set_point_local.publish(self.msgp)
+				# print("Target pose")
+				# print(self.msgp.pose.position.x, self.msgp.pose.position.y, self.msgp.pose.position.z)
+
+		def yawPID(self):
+		
+			Kp = 0.6
+			Kd = 0
+			Ki = 0
+			ERROR_THRESHOLD_FOR_INTEGRATOR = 0.2
+			WINDUP_THRESHOLD = 0.2
+
+			e = self.rel_yaw
+			#print(e*180/3.14)
+			dt = 1
+
+			P = Kp*e
+			if abs(e) < ERROR_THRESHOLD_FOR_INTEGRATOR and abs(self.I) < WINDUP_THRESHOLD:
+				self.I = self.I + Ki*e*(dt)
+			D = Kd*(e - self.e_prev)/(dt)
+
+			Yaw = self.yaw + P + self.I + D
+
+			self.e_prev = e
+			return Yaw
+
+		def setLandMode(self):
+			rospy.wait_for_service('/mavros/set_mode')    
+
+			try:
+					isModeChanged_guided= False 
+					rate2=rospy.Rate(1)
+					while not isModeChanged_guided :            
+							flightModeService = rospy.ServiceProxy('/mavros/set_mode',SetMode)
+							isModeChanged_guided = flightModeService(custom_mode='LAND') #return true or false
+							rospy.loginfo("Flight landing succesfull!!")
+							rate2.sleep()
+			except rospy.ServiceException:
+					print ("service set_mode call failed. LAND Mode could not be set. Check that GPS is enabled")
+
+			
 
 if __name__ == '__main__':
-  try:   
-    moveCopter()
-    rospy.spin()
+	try:   
+		moveCopter()
+		rospy.spin()
 
-  except rospy.ROSInterruptException:
-    pass
-        
+	except rospy.ROSInterruptException:
+		pass
+				
 
-    
+		
 
--- drdo_exploration/scripts/scanner.py
+++ drdo_exploration/scripts/scanner.py
@@ -12,28 +12,7 @@ from std_msgs.msg import Int16
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
-
-x=None
-y=None
-z=None
-p=None
-
-
-def setStabilizeMode():
-    rospy.wait_for_service('/mavros/set_mode')    
-
-    try:
-        isModeChanged_guided= False 
-        rate2=rospy.Rate(1)
-        while not isModeChanged_guided :            
-            flightModeService = rospy.ServiceProxy('/mavros/set_mode',SetMode)
-            isModeChanged_guided = flightModeService(custom_mode='LAND') #return true or false
-            rospy.loginfo("Flight landing succesfull!!")
-            rate2.sleep()
-    except rospy.ServiceException:
-        print "service set_mode call failed. LAND Mode could not be set. Check that GPS is enabled"
-
-
+from drdo_exploration.msg import aruco_detect
 
 def callback_opencv(data):
 	bridge = CvBridge()
@@ -50,27 +29,25 @@ def callback_opencv(data):
 
 	cv2.circle(img,(img.shape[1]//2,img.shape[0]//2),4,(255,0,0),-1)
 
+	aruco = aruco_detect()
+	aruco.cX = 0.0
+	aruco.cY = 0.0
+	aruco.distance = 0.0
+
 	if ids is None: ##If no marker is detected
 		print("FINDING MARKERS!!!")
-		flag_stop = Int16()
-		flag_stop.data = 0
-		pub_stop_explore.publish(flag_stop)
 		cv2.imshow("frame",img)
 		cv2.waitKey(3)
+		aruco.flag = 0
 	else:
 		a = np.where(ids==0)  #tuple containing index of the aruco with id zero.
 		if a[0].size==0:
 			print("2")
-			flag_stop = Int16()
-			flag_stop.data = 0
-			pub_stop_explore.publish(flag_stop)
 			cv2.imshow("frame",img)
 			cv2.waitKey(3)
+			aruco.flag = 0
 		else:
 			print("2")
-			flag_stop = Int16()
-			flag_stop.data = 1
-			pub_stop_explore.publish(flag_stop)
 			cv2.imshow("frame",img)
 			cv2.waitKey(3)
 			
@@ -97,65 +74,22 @@ def callback_opencv(data):
 			cv2.imshow("frame",img)
 			cv2.waitKey(3)
 
-			edge_distance = sqrt( (topLeft[0]-topRight[0])**2 + (topLeft[0]-topRight[0])**2 )//2
+			# edge_distance = sqrt( (topLeft[0]-topRight[0])**2 + (topLeft[0]-topRight[0])**2 )//2
 			distance = 	sqrt( (cX-img.shape[1]//2)**2 + (cY-img.shape[0]//2)**2 )
 
-			limit= 5
+			aruco.flag = 1
+			aruco.cX = cX
+			aruco.cY = cY
+			aruco.distance = distance
 
-			print(distance)
+	pub_aruco_detect.publish(aruco)
 
-			if(distance>100 and distance<300):
-				
-				Delta = 0.1
-
-				pose_msg = PoseStamped()
-				pose_msg.pose.position.x = x + (cX-(img.shape[1]//2))*Delta   
-				pose_msg.pose.position.y= y - (cY-(img.shape[0]//2))*Delta  
-				pose_msg.pose.position.z = z
-				pose_msg.pose.orientation = p
-
-				pub_set_point_local.publish(pose_msg)
-
-			elif(distance>edge_distance and distance<100):
-				
-				Delta = 0.01
-
-				pose_msg = PoseStamped()
-				pose_msg.pose.position.x = x + (cX-(img.shape[1]//2))*Delta   
-				pose_msg.pose.position.y= y - (cY-(img.shape[0]//2))*Delta  
-				pose_msg.pose.position.z = z
-				pose_msg.pose.orientation = p
-
-				pub_set_point_local.publish(pose_msg)
-
-			else:
-				print("Landing")
-				setStabilizeMode() 
-		
-		
-def gps_data_callback(msg):
-	global x,y,z,p
-	''' 
-	msgp is of PoseStamped msg type that we need to publish for going to the goal_point.
-	the use of roll, pitch, and yaw needs to be looked into.
-	''' 
-	x = msg.pose.pose.position.x 
-	y = msg.pose.pose.position.y 
-	z = msg.pose.pose.position.z
-	p = msg.pose.pose.orientation
-	
-
-	
 
 
 if __name__ == '__main__':
 
-	 rospy.init_node('listener', anonymous=True)
-
-	 pub_set_point_local=rospy.Publisher('/mavros/setpoint_position/local', PoseStamped,queue_size=10)
-	 pub_stop_explore = rospy.Publisher("/stop_exploring_flag", Int16,queue_size=10)
-
-	 sub_gps=rospy.Subscriber("/mavros/global_position/local",Odometry, gps_data_callback)
+	 rospy.init_node('aruco_detector', anonymous=True)
+	 pub_aruco_detect = rospy.Publisher("/aruco_detect", aruco_detect,queue_size=10)
 	 rospy.Subscriber("/camera/color/image_raw/", Image, callback_opencv)
 	 
 	 rospy.spin()
